f482180349829e033083415aa124655fcfc93d8c
Archit Mahule <archit.mahule@mathongo.com>
2025-06-07 15:01:09 +0530

    install dependencies

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /home/am/gh/babynama-intern-assignment-template/app/layout.tsx
+// File: D:\College\Projects\babynama-assignment\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
--- .next/types/app/page.ts
+++ .next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/am/gh/babynama-intern-assignment-template/app/page.tsx
+// File: D:\College\Projects\babynama-assignment\app\page.tsx
 import * as entry from '../../../app/page.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
